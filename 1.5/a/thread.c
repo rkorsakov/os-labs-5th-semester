68850753e3c0aab9f8088027d03155c3da7343b0
Roman <roma3io@mail.ru>
2025-10-09 18:55:07 +0700

    better

--- 1.5/a/thread.c
+++ 1.5/a/thread.c
@@ -7,12 +7,16 @@
 #include <signal.h>
 #include <unistd.h>
 
+int sigint_received = 0;
+int sigquit_received = 0;
+
 void sigint_handler(int sig)
 {
     printf("Received SIGINT signal\n");
+    sigint_received = 1;
 }
 
-void *block_signals_thread(void *arg)
+void *thread1(void *arg)
 {
     sigset_t set;
     sigfillset(&set);
@@ -24,22 +28,19 @@ void *block_signals_thread(void *arg)
     return NULL;
 }
 
-void *handle_sigint_thread(void *arg)
+void *thread2(void *arg)
 {
-    if (signal(SIGINT, sigint_handler) == SIG_ERR)
-    {
-        perror("signal");
-        return NULL;
-    }
-
+    signal(SIGINT, sigint_handler);
     while (1)
     {
-        sleep(1);
+        if (sigint_received) {
+            sigint_received = 0;
+        }
     }
     return NULL;
 }
 
-void *handle_sigquit_thread(void *arg)
+void *thread3(void *arg)
 {
     sigset_t wait_set;
     sigemptyset(&wait_set);
@@ -51,6 +52,7 @@ void *handle_sigquit_thread(void *arg)
         if (result == 0 && sig == SIGQUIT)
         {
             printf("Received SIGQUIT signal\n");
+            sigquit_received = 1;
         }
     }
     return NULL;
@@ -63,27 +65,37 @@ int main()
     sigset_t block_set;
     sigaddset(&block_set, SIGQUIT);
     pthread_sigmask(SIG_BLOCK, &block_set, NULL);
-	printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
-    err = pthread_create(&tid1, NULL, block_signals_thread, NULL);
+
+    printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+
+    err = pthread_create(&tid1, NULL, thread1, NULL);
     if (err)
     {
-        printf("main: pthread_create(block_signals_thread) failed: %s\n", strerror(err));
+        printf("main: pthread_create(thread1) failed: %s\n", strerror(err));
         return -1;
     }
 
-    err = pthread_create(&tid2, NULL, handle_sigint_thread, NULL);
+    err = pthread_create(&tid2, NULL, thread2, NULL);
     if (err)
     {
-        printf("main: pthread_create(handle_sigint_thread) failed: %s\n", strerror(err));
+        printf("main: pthread_create(thread2) failed: %s\n", strerror(err));
         return -1;
     }
 
-    err = pthread_create(&tid3, NULL, handle_sigquit_thread, NULL);
+    err = pthread_create(&tid3, NULL, thread3, NULL);
     if (err)
     {
-        printf("main: pthread_create(handle_sigquit_thread) failed: %s\n", strerror(err));
+        printf("main: pthread_create(thread3) failed: %s\n", strerror(err));
         return -1;
     }
+
+    sleep(2);
+    printf("Sending SIGINT...\n");
+    pthread_kill(tid2, SIGINT);
+    
+    sleep(2);
+    printf("Sending SIGQUIT...\n");
+    pthread_kill(tid3, SIGQUIT);
 
     pthread_join(tid1, NULL);
     pthread_join(tid2, NULL);
---
+++ 1.5/b/thread.c
@@ -0,0 +1,110 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+int sigint_received = 0;
+int sigquit_received = 0;
+
+void sigint_handler(int sig)
+{
+    printf("Received SIGINT signal\n");
+    sigint_received = 1;
+}
+
+void sigquit_handler(int sig)
+{
+    printf("Received SIGQUIT signal\n");
+    sigquit_received = 1;
+}
+
+void *thread1(void *arg)
+{
+    sigset_t set;
+    sigfillset(&set);
+    pthread_sigmask(SIG_BLOCK, &set, NULL);
+    while (1)
+    {
+        sleep(1);
+    }
+    return NULL;
+}
+
+void *thread2(void *arg)
+{
+    signal(SIGINT, sigint_handler);
+    while (1)
+    {
+        if (sigint_received) {
+            sigint_received = 0;
+        }
+    }
+    return NULL;
+}
+
+void *thread3(void *arg)
+{
+    signal(SIGQUIT, sigquit_handler);
+    while (1)
+    {
+        if (sigquit_received) {
+            sigquit_received = 0;
+        }
+    }
+    return NULL;
+}
+
+int main()
+{
+    pthread_t tid1, tid2, tid3;
+    int err;
+
+    printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
+
+    err = pthread_create(&tid1, NULL, thread1, NULL);
+    if (err)
+    {
+        printf("main: pthread_create(thread1) failed: %s\n", strerror(err));
+        return -1;
+    }
+
+    err = pthread_create(&tid2, NULL, thread2, NULL);
+    if (err)
+    {
+        printf("main: pthread_create(thread2) failed: %s\n", strerror(err));
+        return -1;
+    }
+
+    err = pthread_create(&tid3, NULL, thread3, NULL);
+    if (err)
+    {
+        printf("main: pthread_create(thread3) failed: %s\n", strerror(err));
+        return -1;
+    }
+
+    sleep(2);
+    printf("Sending SIGINT to thread2...\n");
+    pthread_kill(tid2, SIGINT);
+    
+    sleep(2);
+    printf("Sending SIGQUIT to thread2...\n");
+    pthread_kill(tid2, SIGQUIT);
+
+    sleep(2);
+    printf("Sending SIGQUIT to thread3...\n");
+    pthread_kill(tid3, SIGQUIT);
+
+    sleep(2);
+    printf("Sending SIGINT to thread3...\n");
+    pthread_kill(tid3, SIGINT);
+
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+    pthread_join(tid3, NULL);
+
+    return 0;
+}
