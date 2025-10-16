ab528cd5163917ea1b63f2bf8446a5def6a1ec28
Roman <roma3io@mail.ru>
2025-10-16 10:35:04 +0700

    new test

--- mythread/test.c
+++ mythread/test.c
@@ -4,24 +4,35 @@
 #include <unistd.h>
 #include <string.h>
 
-void *thread1(void* arg) {
-    printf("working...\n");    
-    usleep(3000); 
-    return NULL; 
+void *thread1(void* arg)
+{
+    printf("working...\n");
+    sleep(1);
+    return NULL;
 }
 
-void *thread2(void* arg){
+void *thread2(void* arg)
+{
     char *a = malloc(strlen("hello world") + 1);
-	strcpy(a, "hello world");
-	return a;
+    strcpy(a, "hello world");
+    return a;
 }
 
-int main() {
+void *thread3(void* arg)
+{
+    int *a = (int*)arg;
+    printf("%d\n", *a);
+    return NULL;
+}
+
+int main()
+{
     printf("=== MyThread Test ===\n\n");
     
     printf("1. Default test:\n");
-    mythread t1;    
+    mythread t1;
     int result = mythread_create(&t1, thread1, NULL);
+    
     if (result == MYTHREAD_ERROR) {
         fprintf(stderr, "Error creating thread\n");
         return 1;
@@ -32,19 +43,40 @@ int main() {
         return 1;
     }
     
-    printf("2. Hello world on heap test:\n" );
+    printf("2. Hello world test:\n");
     
     mythread t2;
     result = mythread_create(&t2, thread2, NULL);
+    
     if (result == MYTHREAD_ERROR) {
         fprintf(stderr, "Error creating thread\n");
         return 1;
     }
+    
     char *res;
     if (mythread_join(&t2, (void**)&res) == MYTHREAD_ERROR) {
         fprintf(stderr, "Error joining thread\n");
         return 1;
     }
-	printf("%s\n", res);
+    
+    printf("%s\n", res);
+    free(res);
+
+    printf("3. Passing value test:\n");
+
+    mythread t3;
+    int a = 60;
+    result = mythread_create(&t3, thread3, &a);
+    
+    if (result == MYTHREAD_ERROR) {
+        fprintf(stderr, "Error creating thread\n");
+        return 1;
+    }
+    
+    if (mythread_join(&t3, NULL) == MYTHREAD_ERROR) {
+        fprintf(stderr, "Error joining thread\n");
+        return 1;
+    }
+    
     return 0;
 }
